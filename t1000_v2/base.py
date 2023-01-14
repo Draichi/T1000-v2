5777d5421ad6ae08498400f5e555e2b3529ec6ad
@Draichi <lucasdraichi@gmail.com>
2023-01-13 21:23:23 -0300

    wip: Market abstraction

--- t1000_v2/base.py
+++ t1000_v2/base.py
@@ -12,8 +12,13 @@ If you want to replace this with a Flask application run:
 
 and then choose `flask` as template.
 """
+import os
+
 import gym
 import numpy as np
+import pandas as pd
+import requests
+from dotenv import load_dotenv
 from gym import spaces
 
 CONFIG_SPEC_CONSTANTS = {
@@ -31,17 +36,89 @@ class Renderer:
 
 
 class Market:
+    """An abstraction of a crypto market
+
+    Attributes
+    ----------
+        exchange (str): the exchange name
+        assets (list[str]): a list containing assets to trade"""
+    load_dotenv()
+    CRYPTOCOMPARE_API_KEY = os.getenv('CRYPTOCOMPARE_API_KEY')
     exchange_commission = 0.00075
 
-    def __init__(self) -> None:
+    def __init__(self, exchange: str, assets: list[str], granularity: str, currency: str, datapoints: int) -> None:
         self.df_features = {}
+        self.raw_dataframe = {}
+        self.train_dataframe = {}
+        self.test_dataframe = {}
+        self.exchange = exchange
+        self.granularity = granularity
+        self.currency = currency
+        self.datapoints = datapoints
+        self.get_dataframes(assets)
 
-    def populate_dataframes(self):
-        pass
+    def get_dataframes(self, assets: list[str]):
 
-    @staticmethod
-    def get_data_from_api():
-        pass
+        if not self.CRYPTOCOMPARE_API_KEY:
+            raise ImportError('CRYPTOCOMPARE_API_KEY not found on .env')
+
+        for asset in assets:
+            # fetch api
+            self.raw_dataframe[asset] = self.fetch_api(asset)
+            # add indicators
+            # split into train and test dfs
+            # save to csv files
+            # create key inside self.df with asset's name
+            self.train_dataframe[asset] = {}
+            self.test_dataframe[asset] = {}
+            # read this csv files to avoid ray indexing issue
+
+        print(self.raw_dataframe)
+
+    def fetch_api(self, asset: str) -> pd.DataFrame:
+        """Fetch the CryptoCompare API and return historical prices for a given asset
+
+            Parameters:
+                asset (str): The asset name. For a full asset list check: https://min-api.cryptocompare.com/documentation?key=Other&cat=allCoinsWithContentEndpoint
+
+            Returns:
+                raw_dataframe (pandas.Dataframe): The API 'Data' key response converted to a pandas Dataframe
+        """
+
+        print('> Fetching {} dataset'.format(asset))
+
+        path_to_raw_dataframe = 'data/raw_dataframe_{}.csv'.format(
+            asset)
+
+        if os.path.exists(path_to_raw_dataframe):
+            raw_dataframe = pd.read_csv(path_to_raw_dataframe)
+
+            return raw_dataframe
+
+        else:
+            headers = {'User-Agent': 'Mozilla/5.0',
+                       'authorization': 'Apikey {}'.format(self.CRYPTOCOMPARE_API_KEY)}
+            url = 'https://min-api.cryptocompare.com/data/histo{}?fsym={}&tsym={}&limit={}&e={}'.format(
+                self.granularity, asset, self.currency, self.datapoints, self.exchange)
+            response = requests.get(url, headers=headers)
+            json_response = response.json()
+            status = json_response['Response']
+
+            if status == "Error":
+                print('Error fetching {} dataframe'.format(asset))
+                raise AssertionError(json_response['Message'])
+
+            result = json_response['Data']
+            raw_dataframe = pd.DataFrame(result)
+
+            to_datetime_arg = raw_dataframe['time']
+            raw_dataframe.drop(['time', 'conversionType',
+                               'conversionSymbol'], axis=1, inplace=True)
+
+            raw_dataframe['Date'] = pd.to_datetime(
+                arg=to_datetime_arg, utc=True, unit='s')
+
+            return raw_dataframe
 
 
 class Wallet:
@@ -56,30 +133,31 @@ class Wallet:
 
 
 class ExchangeEnvironment(gym.Env, Wallet, Market):
-    def __init__(self, net_worth: int, balance: int, assets: list) -> None:
+    def __init__(self, net_worth: int, balance: int, assets: list, currency: str, exchange: str, granularity: str, datapoints: int) -> None:
         Wallet.__init__(self, net_worth, balance)
-        Market.__init__(self)
+        Market.__init__(self, exchange=exchange, assets=assets, currency=currency,
+                        datapoints=datapoints, granularity=granularity)
 
         # 4,3 = (balance, cost, sales, net_worth) + (shares bought, shares sold, shares held foreach asset)
-        observation_length = len(self.df_features[assets[0]].columns) + 4 + 3
+        # observation_length = len(self.df_features[assets[0]].columns) + 4 + 3
 
         # action space = buy and sell for each asset, pĺus hold position
-        action_space = 1 + len(assets) * 2
+        # action_space = 1 + len(assets) * 2
 
         # obs space = (num assets, indicator + (balance, cost, sales, net_worth) + (shares bought, shares sold, shares held foreach asset))
-        observation_space = (len(assets),
-                             observation_length)
+        # observation_space = (len(assets),
+        #                      observation_length)
 
-        self.action_space = spaces.Box(
-            low=np.array([0, 0]),
-            high=np.array([action_space, 1]),
-            dtype=np.float16)
+        # self.action_space = spaces.Box(
+        #     low=np.array([0, 0]),
+        #     high=np.array([action_space, 1]),
+        #     dtype=np.float16)
 
-        self.observation_space = spaces.Box(
-            low=-np.finfo(np.float32).max,
-            high=np.finfo(np.float32).max,
-            shape=observation_space,
-            dtype=np.float16)
+        # self.observation_space = spaces.Box(
+        #     low=-np.finfo(np.float32).max,
+        #     high=np.finfo(np.float32).max,
+        #     shape=observation_space,
+        #     dtype=np.float16)
 
     def get_HODL_strategy(self) -> None:
         pass
@@ -144,9 +222,9 @@ class TradingFloor(ExchangeEnvironment, Brain, Renderer):
 
     """
 
-    def __init__(self, assets: list, checkpoint_path='', algorithm='PPO', currency='USD', granularity='hour', datapoints=150, initial_balance=300, exchange_commission=0.00075, exchange='binance') -> None:
+    def __init__(self, assets: list[str], checkpoint_path='', algorithm='PPO', currency='USD', granularity='hour', datapoints=150, initial_balance=300, exchange_commission=0.00075, exchange='CCCAGG') -> None:
         ExchangeEnvironment.__init__(
-            self, initial_balance, initial_balance, assets)
+            self, net_worth=initial_balance, assets=assets, currency=currency, exchange=exchange, granularity=granularity, datapoints=datapoints, balance=initial_balance)
         Brain.__init__(self, 1e-4)
         Renderer.__init__(self)
         self.assets = assets
@@ -171,3 +249,6 @@ class TradingFloor(ExchangeEnvironment, Brain, Renderer):
             raise ValueError("Incorrect 'granularity' value")
         if type(self.datapoints) != int or 1 > self.datapoints > 2000:
             raise ValueError("Incorrect 'datapoints' value")
+
+
+T1000 = TradingFloor(['BTC'])
